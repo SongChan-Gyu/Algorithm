069570c8be0446ad0ad8a4e169ba558e723a072e
SongChan-Gyu <zx2253@naver.com>
2020-01-08 01:59:00 +0900

    2020-01-08 greedy 연습

---
+++ src/Greedy/Greedy11047.java
@@ -0,0 +1,17 @@
+package Greedy;
+
+public class Greedy11047 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int N=10;
+		int K=4200;
+		int A[] = {1,5,10,50,100,500,1000,5000,10000,50000};
+		
+		for(int i=A.length;i>=0;i--) {
+			
+		}
+		
+	}
+
+}
